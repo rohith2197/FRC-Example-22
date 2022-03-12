a1956a34b0b46eda374c570be58779a1d4193487
Rohith2197 <57274135+rohith2197@users.noreply.github.com>
2022-03-11 21:34:10 -0500

    Updated drive subsystem for trajectories
    Added the required constants like encoded to the constants file, and updated the drive subsystem to fit the trajectories

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,23 +13,46 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class DriveConstants{
+        public static final int kLefMotor1Port = 3;
+        public static final int kLefMotor2Port = 5;
 
-    // Drivetrain Constants
-    public static final class DriveConstants {
-        
-        // Motor ports location on CAN thingamabobber
-        public static final int kLeftMotor1Port = 5;
-        public static final int kLeftMotor2Port = 7;
-
-        public static final int kRightMotor1Port = 6;
+        public static final int kRightMotor1Port = 4;
         public static final int kRightMotor2Port = 8;
 
-        // Extra motors
-        // public static final int kMotor1 = 11;
-    }
+        public static final int kShooter1 = 11;
+        //constants for each motor (k shooter is for the shooter)
 
-    // Controller constants
-    public static final class OIConstants {
+        public static final double ksVolts = 0.4;
+        public static final double kvVoltSecondsPerMeter = 1.5;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.3;
+
+        public static final double kPDriveVel = 5.5;
+        //constants for feedforward
+
+        public static final double kTrackWidthMeters = 0.64;
+        public static final DifferentialDriveKinematics kDriveKinematics
+                = new DifferentialDriveKinematics(kTrackWidthMeters);
+        //constants for trackwidth
+
+        public static final double kMaxSpeed = 4;
+        //meters per second
+        public static final double kMaxAcceleration = 4;
+        //meters per second squared
+
+        //constants for max speed/acceleration
+
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;
+        //constants for ramsete controller
+
+        public static final int[] kLeftEncoderPorts = {6, 7};
+        public static final int[] kRightEncoderPorts = {9, 10};
+
+        public static final double kEncoderDistancePerPulse = 0.2;
+
+    }
+    public static final class OIConstants{
         public static final int controller = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,41 +4,106 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
-public class DriveSubsystem extends SubsystemBase {
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
-    // Declaring Initializing Drivetrain Motor Groups
-    private final MotorControllerGroup leftMotors = new MotorControllerGroup(
-        new CANSparkMax(DriveConstants.kLeftMotor1Port, MotorType.kBrushless),
-        new CANSparkMax(DriveConstants.kLeftMotor2Port, MotorType.kBrushless));
+import edu.wpi.first.wpilibj.examples.ramsetecommand.Constants.DriveConstants;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.Encoder;
 
-    private final MotorControllerGroup rightMotors = new MotorControllerGroup(
-        new CANSparkMax(DriveConstants.kRightMotor1Port, MotorType.kBrushless),
-        new CANSparkMax(DriveConstants.kRightMotor2Port, MotorType.kBrushless));
+import edu.wpi.first.math.geometry.Pose2d;
 
-    
-    // Declaring differential drivetrain using motor groups
+public class DriveSubsystem extends SubsystemBase{
+    private final MotorControllerGroup leftMotors =
+            new MotorControllerGroup(new CANSparkMax(DriveConstants.kLefMotor1Port, MotorType.kBrushless),
+                    new CANSparkMax(DriveConstants.kLefMotor2Port, MotorType.kBrushless));
+
+    private final MotorControllerGroup rightMotors =
+            new MotorControllerGroup(new CANSparkMax(DriveConstants.kRightMotor1Port, MotorType.kBrushless),
+                    new CANSparkMax(DriveConstants.kRightMotor2Port, MotorType.kBrushless));
+
     private final DifferentialDrive diffDrive = new DifferentialDrive(leftMotors, rightMotors);
 
-    /**
-     * Inverts right motors, as per WPILib convention.
-     */
-    public DriveSubsystem() {
+
+    private final Encoder leftEncoder = new Encoder
+            (DriveConstants.kLeftEncoderPorts[0],
+                    DriveConstants.kLeftEncoderPorts[1]);
+
+    private final Encoder rightEncoder = new Encoder
+            (DriveConstants.kRightEncoderPorts[0],
+                    DriveConstants.kRightEncoderPorts[1], true);
+
+    private final Gyro gyro = new ADXRS450_Gyro();
+    private final DifferentialDriveOdometry odometry;
+
+
+    public DriveSubsystem(){
         rightMotors.setInverted(true);
+
+        leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+        rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+
+        resetEncoders();
+        odometry = new DifferentialDriveOdometry(gyro.getRotation2d());
+    }
+    @Override
+    public void periodic(){
+        odometry.update(gyro.getPosition2d, leftEncoder.getDistance(), rightEncoder.getDistance());
     }
 
-    /**
-     * Allows the robot to drive using arcade drive based upon input parameters 
-     * 
-     * @param xSpeed The forward and backward speed of robot
-     * @param rotation The turning input for the robot 
-     */
-
-    public void drive(double xSpeed, double rotation) {
-        diffDrive.arcadeDrive(xSpeed, rotation, true); 
+    public Pose2d getPose(){
+        return odometry.getPoseMeters();
     }
-        
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds(){
+        return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());
+    }
+
+    public void resetOdometry(Pose2d pose){
+        resetEncoders();
+        odometry.resetPosition(pose, gyro.getRotation2d());
+    }
+
+    public void drive(double xSpeed, double rotation){
+        diffDrive.arcadeDrive(xSpeed, rotation, true);
+    }
+
+    public void resetEncoders(){
+        leftEncoder.reset();
+        rightEncoder.reset();
+    }
+
+    public double getAverageEncoderDistance(){
+        return (leftEncoder.getDistance() + rightEncoder.getDistance()) / 2.0;
+    }
+
+    public Encoder getLeftEncoder(){
+        return leftEncoder;
+    }
+
+    public Encoder getRightEncoder(){
+        return rightEncoder;
+    }
+
+    public void setMaxOutput(double maxOutput){
+        drive.setMaxOutput(maxOutput);
+    }
+
+    public void zeroHeading(){
+        gyro.reset();
+    }
+
+    public double getHeading(){
+        return gyro.getRotation2d().getDegrees();
+    }
+
+    public double getTurnRate(){
+        return -gyro.getRate();
+    }
 }
